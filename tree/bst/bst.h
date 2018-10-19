c622a39f12ed71eb1a06e3b83410cad3b8f2f330
Caiden Pyle <40413115+Caidentp@users.noreply.github.com>
2018-10-19 09:01:06 -0500

    Update bst.h

--- tree/bst/bst.h
+++ tree/bst/bst.h
@@ -16,13 +16,13 @@ class Node
         Node(int data, Node* parent);
 
         int get_data() const;
-        void set_data(const int new_data);
+        void set_data(int new_data);
         Node* get_parent() const;
-        void set_parent(const Node* new_parent);
+        void set_parent(Node* new_parent);
         Node* get_left() const;
-        void set_left(const Node* new_left);
+        void set_left(Node* new_left);
         Node* get_right() const;
-        void set_right(const Node* new_right);
+        void set_right(Node* new_right);
 
         void insertNode(const int data);
         void deleteNode(const int data);
@@ -30,7 +30,7 @@ class Node
         void preorder() const;
         void inorder() const;
         void postorder() const;
-        bool contains(const int data);
+        bool contains(const int data) const;
         int child_count() const;
 
         virtual ~Node();
