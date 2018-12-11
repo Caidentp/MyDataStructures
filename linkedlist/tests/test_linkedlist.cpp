c10a3eba86c18618e58a97947364c9bdc6ca6e1f
Caiden Pyle <40413115+Caidentp@users.noreply.github.com>
2018-12-11 14:26:15 -0600

    Test push and append for empty lists
    Added testing for pushing and appending data to empty linked
    lists.

---
+++ linkedlist/tests/test_linkedlist.cpp
@@ -0,0 +1,40 @@
+#include <cassert>
+#include "linkedlist_testing.h"
+
+
+void test_push_empty_list() {
+	sList singly = sList();
+	dList doubly = dList();
+	cList circular = cList();
+	singly.push(1);
+	doubly.push(1);
+	circular.push(1);
+	assert(singly.get_head()->data == 1);
+	assert(singly.get_tail()->data == 1);
+	assert(doubly.get_head()->data == 1);
+	assert(doubly.get_tail()->data == 1);
+	assert(circular.get_head()->data == 1);
+	assert(circular.get_tail()->data == 1);
+}
+
+
+void test_append_empty_list() {
+	sList singly = sList();
+	dList doubly = dList();
+	cList circular = cList();
+	singly.append(1);
+	doubly.append(1);
+	circular.append(1);
+	assert(singly.get_head()->data == 1);
+	assert(singly.get_tail()->data == 1);
+	assert(doubly.get_head()->data == 1);
+	assert(doubly.get_tail()->data == 1);
+	assert(circular.get_head()->data == 1);
+	assert(circular.get_tail()->data == 1);
+}
+
+
+int main() {
+	test_push_empty_list();
+	test_append_empty_list();
+}
