1f0855924bc962f337d3c1b823c05a32d1d73bd2
Caiden <caid.t.pyle@gmail.com>
2018-12-12 18:12:59 -0600

    Added tests to tree/bst/test_bst.cpp
    Added tests to test_bst.cpp to test all current member functions.
    Tests now include test_insert(), test_contains(), test_find(),
    test_parent(), and test_children().

--- tree/bst/test_bst.cpp
+++ tree/bst/test_bst.cpp
@@ -11,14 +11,57 @@ void test_insert() {
     test.insert(1);
     test.insert(2);
     test.insert(0);
-    assert(test.get_data() == 1);
-    assert(test.get_left()->get_data() == 0);
-    assert(test.get_right()->get_data() == 2);
+    assert(test.get_data() == 1 && "test_insert()");
+    assert(test.get_left()->get_data() == 0 && "test_insert()");
+    assert(test.get_right()->get_data() == 2 && "test_insert()");
+}
+
+
+void test_contains() {
+    tree test = tree();
+    test.insert(1);
+    assert(test.contains(1) == true && "test_contains()");
+    assert(test.contains(2) == false && "test_contains()");
+}
+
+
+void test_find() {
+    tree test = tree();
+    test.insert(1);
+    assert(test.find(1)->get_data() == 1 && "test_find()");
+    assert(test.find(2) == nullptr && "test_find()");
+}
+
+
+void test_parent() {
+    tree test = tree();
+    test.insert(1);
+    test.insert(2);
+    assert(test.parent(1) == nullptr && "test_parent()");
+    assert(test.parent(2)->get_data() == 1 && "test_parent()");
+}
+
+
+void test_children() {
+    tree test = tree();
+    test.insert(5);
+    test.insert(2);
+    test.insert(1);
+    test.insert(3);
+    assert(test.find(5)->children() == 1 && "test_children()");
+    assert(test.find(2)->children() == 2 && "test_children()");
+    assert(test.find(1)->children() == 0 && "test_children()");
 }
 
 
 int main() {
     test_insert();
+    test_contains();
+    test_find();
+    test_parent();
+    test_children();
+
     std::cout << std::endl << "All tests passed." << std::endl;
     return 0;
 }
+
