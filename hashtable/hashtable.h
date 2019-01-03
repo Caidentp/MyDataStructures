f913d58f044cab189c08ff1ab32995220ae82bde
Caiden Pyle <40413115+Caidentp@users.noreply.github.com>
2019-01-03 13:32:32 -0600

    Update hashtable.h

--- hashtable/hashtable.h
+++ hashtable/hashtable.h
@@ -1,40 +1,48 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+#include <exception>
+#include <string>
 
 namespace hashtable {
 
-/** Hash table of key-value mappings. Avoids collisions by linear
- * probing with linked lists.
+/**
+ *  @brief  Hash table of key-value mappings where keys are strings.
+ *  @var  table  Array of HashTable::LinkedList objects.
+ *  @var  size  Size of table array.
  *
- * template V : data type that values should be for key-value pairs.
- *      - Defaults to int.
+ *  @tparam  V  data type for values.
  *
- * @param table : Array of HashTable::LinkedList objects.
- * @param size : Size of table array.
+ *  Flexible data structure where values are easily accesed by their keys.
+ *  Values can be a data type of your choice. Values are accesed in O(1)
+ *  best and average case and O(n) worst case where n is the length of the
+ *  linked list at the index returned by the hash function.
  */
 template <class V = int>
-class HashTable
-{
+class HashTable {
     // Keys are strings
-    typedef char* K;
+    typedef std::string K;
 
+#ifndef TESTING
     private:
-        /** Linked list class used for collision avoidance.
-         *
-         * @param head : Pointer to first node in list.
-         * @param tail : Pointer to last node in list.
-         */
-        class LinkedList
-        {
+#endif
+#ifdef TESTING
+    public:
+#endif
+        /** 
+          *  @brief  Linked list class used for collision avoidance.
+          *  @var  head  Pointer to first node in list.
+          *  @var  tail  Pointer to last node in list.
+          */
+        class LinkedList {
             public:
-                /** Nodes for linked lists have keys and values.
-                 * @param key : Key for looking up value.
-                 * @param val : Value that can be looked up by key.
-                 * @param next : Pointer to next node in list.
-                 */
-                class Node
-                {
+                /** 
+                   *  @brief  Nodes for linked lists have keys and values.
+                   *  @var  key  Key for looking up value.
+                   *  @var  val  Value that can be looked up by key.
+                   *  @var  next  Pointer to next node in list.
+                   */
+                class Node {
                     public:
                         K key;
                         V val;
@@ -43,91 +51,132 @@ class HashTable
                             : key(key)
                             , val(val)
                             , next(next) { }
-
                 };  /// HashTable::LinkedList::Node
 
                 Node *head;
                 Node *tail;
 
             public:
-                LinkedList() : head(nullptr) { }
-                ~LinkedList() { delete head; delete tail; }
+                LinkedList() : head(nullptr), tail(nullptr) { }
+                ~LinkedList();
 
-                /** Insert a node into a linked list. If key already exists
-                 * in list, update val instance variable of node.
-                 *
-                 * @param key : Key to lookup value by.
-                 * @param val : Value to be looked up by key.
-                 */
+                /** 
+                   *  @brief  Insert a node into a linked list. 
+                   *  @param  key  Key to lookup value by.
+                   *  @param  val  Value to be looked up by key.
+                   *
+                   *  If the key already exists in the list, the value instance
+                   *  variable of the node will be updated to contain the new val.
+                   */
                 void insert(const K key, const V val);
 
                 /** Find a node's value by its key.
-                 *
-                 * @param key : Key to lookup node by.
-                 * @return Value of key if key exists in list, NULL otherwise.
-                 */
+                   *
+                   * @param key : Key to lookup node by.
+                   * @return Value of key if key exists in list, NULL otherwise.
+                   */
                 V find(const K key) const;
 
-                /** Remove a node from a linked list if it exists.
-                 *
-                 * @param key : Key of node to remove from list.
-                 */
+                /** 
+                   *  @brief  Remove a node from a linked list if it exists.
+                   *  @param  key  Key of node to remove from list.
+                   */
                 void remove(const K key);
-
         };  /// HashTable::LinkedList
 
-
+#ifndef TESTING
     private:
+#endif
+#ifdef TESTING
+    public:
+#endif
         LinkedList *table;
         int size;
 
+#ifndef TESTING
     protected:
-        /** Return a valid index in range of 0 to this->size.
-         */
-        int hash(const K key) {
-            int index = 0;
-            int probe = 0;
-            for (int i = 0; key[i] != '\0'; i++)
-                index += (++probe)*key[i];
-            return index % size;
-        }
+#endif
+#ifdef TESTING
+    public:
+#endif
+        /** 
+          *  @brief  Return a valid index in range of 0 to this->size.
+          *  @param  key  Hash is calculated by the key passed to the function.
+          */
+        inline int hash(const K key);
 
     public:
         HashTable(const int size = 127) : size(size) { table = new LinkedList[size]; }
         ~HashTable() { delete [] table; }
 
-        /** Put a key-value pair into the hash table. If key already
-         * exists, value will be written over.
-         *
-         * @param key : Key to lookup value by.
-         * @param val : Value to be looked up by key.
-         */
+        /** 
+          *  @brief  Put a key-value pair into the hash table. If key already
+          *          exists, value will be written over.
+          *  @param  key  Key to lookup value by.
+          *  @param  val  Value to be looked up by key.
+          */
         void put(const K key, const V val) {
-            int index = this->hash(key);
-            table[index].insert(key, val);
+            table[this->hash(key)].insert(key, val);
         }
 
-        /** Retrieve a value by its key if it exists in the table.
-         *
-         * @param key : Key of value to find.
-         * @return Value that matches key if it exists.
-         */
+        /** 
+          *  @brief  Retrieve a value by its key if it exists in the table.
+          *  @param  key  Key of value to find.
+          *  @return Value that matches key if it exists.
+          */
         V get(const K key) {
-            int index = this->hash(key);
-            return table[index].find(key);
+            return table[this->hash(key)].find(key);
         }
 
-        /** Remove a key-value pair from the table by key.
-         *
-         * @param key : Key of key-value pair to remove from table.
-         */
+        /** 
+          *  @brief  Remove a key-value pair from the table by key.
+          *  @param  key  Key of key-value pair to remove from table.
+          */
         void remove(const K key) {
-            int index = this->hash(key);
-            table[index].remove(key);
+            table[this->hash(key)].remove(key);
         }
-
 };  /// HashTable
 
+
+struct KeyError : public std::exception {
+    const char *what() const throw() {
+        return "Key does not exist.";
+    }
+};
+
+
+// ======================= HashTable =======================
+
+template <class V>
+inline int HashTable<V>::hash(const K key) {
+    int index = 0;
+    // Probe is used so that permutations of the same key will
+    // generate different hashes
+    int probe = 0;
+    for (int i = 0; key[i] != '\0'; i++) {
+        index += (++probe)*key[i];
+    }
+    // Ensure index is in range of LinkedList array
+    return index % size;
+}
+
+// ======================= HashTable =======================
+
+
+// ================= HashTable::LinkedList =================
+
+template <class V>
+HashTable<V>::LinkedList::~LinkedList() {
+    Node* temp = head;
+    Node* curr = head;
+    while (temp != nullptr) {
+        temp = temp->next;
+        delete curr;
+        curr = temp;
+    }
+    head = nullptr;
+    tail = nullptr;
+}
 
 /// Insert a node into a linked list
 template <class V>
@@ -155,16 +204,22 @@ void HashTable<V>::LinkedList::insert(const K key, const V val) {
 /// Find a node's value by its key.
 template <class V>
 V HashTable<V>::LinkedList::find(const K key) const {
-    for (Node *i = head; i != nullptr; i = i->next)
-        if (i->key == key)
+    for (Node *i = head; i != nullptr; i = i->next) {
+        if (i->key == key) {
             return i->val;
-    return NULL;
+        }
+    }
+    throw KeyError();
 }
 
 
 /// Delete a node from a linked list.
 template <class V>
 void HashTable<V>::LinkedList::remove(const K key) {
+    // if table is empty
+    if (head == nullptr) {
+        return;
+    }
     // if head is to be deleted
     if (head->key == key) {
         // if head is last node in list
@@ -172,35 +227,39 @@ void HashTable<V>::LinkedList::remove(const K key) {
             head = nullptr;
             tail = nullptr;
         }
-        else
+        else {
+            Node *temp = head;
             head = head->next;
+            delete temp;
+        }
     }
     else {
         Node *prev = head;
         Node *curr = head->next;
-
         for (; curr != nullptr && curr->next != nullptr;) {
-            if (curr->key == key)
+            if (curr->key == key) {
                 break;
+            }
             prev = curr;
             curr = curr->next;
         }
-
         // if node does not exist in list
-        if (curr == nullptr)
+        if (curr == nullptr) {
             return;
+        }
         // if node to be deleted is tail
-        else if (curr->next == nullptr && curr->key == key)
+        else if (curr->next == nullptr && curr->key == key) {
             tail = prev;
+        }
         prev->next = curr->next;
-        curr->next = nullptr;
+        delete curr;
     }
 }
 
+// ================= HashTable::LinkedList =================
+
 }  /// namespace hashtable
 
-
-typedef hashtable::HashTable<> HashTable;
-
+typedef hashtable::HashTable<int> table;
 
 #endif  /// HASHTABLE_H
