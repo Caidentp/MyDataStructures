cf2137eb6ac496736ed210699794c05d72b06853
Pyle <v31353@txt.textron.com>
2019-01-11 12:48:42 -0600

    Updated documentaion of queue.h

--- queue/queue.h
+++ queue/queue.h
@@ -1,15 +1,29 @@
 #ifndef MYQUEUE_H
 #define MYQUEUE_H
 
-
 namespace myqueue {
 
+/**
+ *  @brief  Queue implimentation using a linked list.
+ *  @var  head  Pointer to the first node in the list.
+ *  @var  tail  Pointer to the last node in the list.
+ *
+ *  @tparam  T  Data type held by queue.
+ *
+ *  This class is a linked list that acts as a queue. New nodes
+ *  are inserted at the front of the linked list and nodes are removed
+ *  from the rear of the linked list. Insert and remove methods
+ *  operate in O(1) time.
+ */
 template <class T = int>
-class Queue
-{
+class Queue {
     private:
-        class Node
-        {
+	    /**
+		 *  @brief  Doubly linked list node to hold queue data variables.
+		 *  @var  next  Pointer to next node in the list.
+		 *  @var  prev  Pointer to the previous node in the list.
+		 */
+        class Node {
             public:
                 T data;
                 Node *next;
@@ -24,45 +38,87 @@ class Queue
 
     public:
         Queue() : head(nullptr) , tail(nullptr) { }
-        ~Queue() { delete head; }
+        ~Queue();
 
-        void enqueue(const T data) {
-            if (head == nullptr) {
-                head = new Node(data);
-                tail = head;
-            }
-            else {
-                tail->next = new Node(data);
-                tail = tail->next;
-            }
-        }
+        /**
+		 *  @brief  Insert an item into the queue.
+		 *  @param  data  Data to insert into the queue.
+		 */
+        inline void enqueue(const T data);
+		
+		/**
+		 *  @brief  Remove and return top node in queue.
+		 *  @return Data instance variable of top node.
+		 */
+        inline T dequeue();
+		
+		/**
+		 *  @brief  See top item in queue without removing it.
+		 *  @return Data instance variable of top node.
+		 */
+        T peek() const;
 
-        T dequeue() {
-            if (tail != nullptr) {
-                T data = head->data;
-                if (head == tail) {
-                    head = nullptr;
-                    tail = nullptr;
-                }
-                else
-                    head = head->next;
-                return data;
-            }
-            return NULL;
-        }
-
-        T peek() const {
-            if (head != nullptr)
-                return head->data;
-            return NULL;
-        }
-
-        bool empty() const {
-            return head == nullptr;
-        }
+        /**
+		 *  @brief  Check whether the queue is empty or not.
+		 *  @return True if queue is emtty, false otherwise.
+		 */
+        bool empty() const { return head == nullptr; }
 };  /// Queue
 
-}  /// namespace myqueue
 
+template <class T>
+inline void Queue<T>::enqueue(const T data) {
+	if (head == nullptr) {
+		head = new Node(data);
+		tail = head;
+	}
+	else {
+		tail->next = new Node(data);
+		tail = tail->next;
+	}
+}
+
+
+template <class T>
+Queue<T>::~Queue() {
+	Node<T>* temp = head;
+	while (temp) {
+		Node<T>* previous = temp;
+		temp = temp->next;
+		delete previous;
+	}
+	head = nullptr;
+	tail = nullptr;
+}
+
+
+template <class T>
+inline T Queue<T>::dequeue() {
+	if (tail != nullptr) {
+		Node<T>* temp = head;
+		T data = temp->data;
+		if (head == tail) {
+			head = nullptr;
+			tail = nullptr;
+		}
+		else {
+			head = head->next;
+		}
+		delete temp;
+		return data;
+	}
+	return NULL;
+}
+
+
+template <class T>
+inline T Queue<T>::peek() const {
+	if (head != nullptr) {
+		return head->data;
+	}
+	return NULL;
+}
+
+}  /// namespace myqueue
 
 #endif  /// MYQUEUE_H
