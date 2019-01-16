b4cd7bedf5fe2888817ed26c04de41f2260304e5
Caiden Pyle <caid.t.pyle@gmail.com>
2019-01-16 10:43:07 -0600

    Added scripts for testing unrolledLinkedListStack

--- stack/unrolledLinkedListStack/stack.h
+++ stack/unrolledLinkedListStack/stack.h
@@ -1,80 +1,89 @@
 #ifndef UNROLLED_LINKED_LIST_STACK_H
 #define UNROLLED_LINKED_LIST_STACK_H
 
+namespace ustack {
 
+#include <exception>
 
-/** Linked list node.
+/** 
+ *  @brief  Unrolled linked list stack class.
+ *  @var  head  Head node of the linked list.
  *
- * @var array : Data member of unrolled linked list node.
- * @var next : Pointer to next node.
- * @var top : Keep track of the last node in the array.
- * @var previous : Pointer to previous node.
+ *  @tparam T Data held by stack nodes.
+ *
+ *  An unrolled stack is linked list stack whose
+ *  nodes contain arrays of data rather than a single
+ *  data instance variable. Unrolled linked list stacks
+ *  benifit from the speed of access of array stacks
+ *  as well as the dynamic nature of linked lists.
  */
 template <class T = int>
-class Node
-{
-    static const short SIZE = 10;
-
+class Stack {
     public:
-        T array[SIZE];
-        int top = 0;
-        Node* next = nullptr;
-        Node* previous = nullptr;
-};
+        /** 
+         *  @brief  Linked list node.
+         *  @var  array  Data member of unrolled linked list node.
+         *  @var  next   Pointer to next node.
+         *  @var  top    Keep track of the last node in the array.
+         *  @var  previous  Pointer to previous node.
+         */
+        class Node {
+            static const short SIZE = 10;
 
+            public:
+                T array[SIZE];
+                int top{0};
+                Node* next{nullptr};
+                Node* previous{nullptr};
+        };
+    
+        Node* head;
 
-
-/** Unrolled linked list stack class.
- *
- * @var head : Head node of the linked list.
- */
-template <class T = int>
-class Stack
-{
-    public:
-        Node<T>* head;
-
-    public:
         Stack() : head(nullptr) { }
 
-        /** Returns true if stack has 0 members.
+        /** 
+         *  @brief  Returns true if stack has 0 members.
+         *  @return True if stack is empty, false otherwise.
          */
         bool empty() { return head == nullptr; }
-        
-        /** Add a data member to the top of the stack. Create new node
-         * if head node's array is full.
-         *
-         * @param data : Data to add to the stack.
+
+        /** 
+         *  @brief  Add a data member to the top of the stack.
+         *  @param  data  Data to add to the stack.
          */
         void push(const T data);
-        
-        /** Remove the top of the stack and return it. Updates the head
-         * node if the current head node's array is empty.
-         *
-         * @return : Data that resides at the top of the stack.
+
+        /** 
+         *  @brief  Remove the top of the stack and return it.
+         *  @return Data that resides at the top of the stack.
          */
         T pop();
-        
-        /** Return the top of the stack without removing it from the stack.
-         *
-         * @return : Data that resides at the top of the stack.
+
+        /** 
+         *  @brief  Return the top of the stack without removing it from the stack.
+         *  @return Data that resides at the top of the stack.
          */
-        T peek();
+        T peek() const;
 };
 
+
+struct EmptyStack : public std::exception {
+    const char *what() const throw() {
+        return "Stack is empty.";
+    }
+};
 
 
 template <class T>
 void Stack<T>::push(const T data) {
-    if (head == nullptr)
-        head = new Node<T>();
-
+    if (head == nullptr) {
+        head = new Node();
+    }
     else if (head->top == 10) {
         head->previous = new Node<T>();
         head->previous->next = head;
         head = head->previous;
     }
-
     head->array[head->top++] = data;
 }
 
@@ -83,24 +92,26 @@ template <class T>
 T Stack<T>::pop() {
     if (head != nullptr) {
         T data = head->array[--head->top];
-
         if (head->top == 0) {
             head = head->next;
-
-            if (head != nullptr)
+            if (head != nullptr) {
                 head->previous = nullptr;
+            }
         }
         return data;
     }
+    throw EmptyStack();
 }
 
 
 template <class T>
-T Stack<T>::peek() {
-    if (head != nullptr)
+T Stack<T>::peek() const {
+    if (head != nullptr) {
         return head->array[head->top-1];
+    }
+    throw EmptyStack();
 }
 
+}  ///  namespace ustack
 
-
-#endif // UNROLLED_LINKED_LIST_STACK_H
+#endif /// UNROLLED_LINKED_LIST_STACK_H
